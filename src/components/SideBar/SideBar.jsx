eeccaf0d5675c212fe98f25fb266fd0ecb2fdd4e
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-23 06:03:11 +0530

    add logo container

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import logo from "../../imgs/logo.png";
 import "./sidebar.css";
 
 const SideBar = () => {
-	return <div></div>;
+	return (
+		<div className="SiderBar">
+			<div className="logo">
+				<img alt="logo img" src={logo} />
+				<span>
+					Sh<span>o</span>ps
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default SideBar;
