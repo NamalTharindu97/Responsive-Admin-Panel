2cd1e0e3f7734e176b1a708fd5801fa3feda1930
NamalTharindu97 <NamalA701997@gmail.com>
2023-05-23 06:54:18 +0530

    add icons

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 import "./sidebar.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 const SideBar = () => {
 	return (
 		<div className="SiderBar">
+			{/* logo */}
 			<div className="logo">
 				<img alt="logo img" src={logo} />
 				<span>
 					Sh<span>o</span>ps
 				</span>
+			</div>
+			{/* menu */}
+			<div className="menu">
+				<div className="menu-Item">
+					<div>
+						<UilReact />
+					</div>
+					<span>Dashboard</span>
+				</div>
+				<div className="menu-Item">
+					<div>
+						<UilReact />
+					</div>
+					<span>Dashboard</span>
+				</div>
 			</div>
 		</div>
 	);
