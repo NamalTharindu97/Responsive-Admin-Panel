7745505d289e424fe4713db2c963425338af384f
NamalTharindu97 <NamalA701997@gmail.com>
2023-06-03 23:32:42 +0530

    create status color change option

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -19,11 +19,33 @@ const rows = [
 	createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
 ];
 
+const makeStyle = (status) => {
+	if (status === "Approved") {
+		return {
+			background: "rgb(145 254 159 / 47%)",
+			color: "green",
+		};
+	} else if (status === "Pending") {
+		return {
+			background: "#ffadad8f",
+			color: "red",
+		};
+	} else {
+		return {
+			background: "#59bfff",
+			color: "white",
+		};
+	}
+};
+
 const BasicTable = () => {
 	return (
 		<div className="Table">
 			<h3>Recent Orders</h3>
-			<TableContainer component={Paper}>
+			<TableContainer
+				component={Paper}
+				style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+			>
 				<Table sx={{ minWidth: 650 }} aria-label="simple table">
 					<TableHead>
 						<TableRow>
@@ -51,7 +73,14 @@ const BasicTable = () => {
 									{row.trackingId}
 								</TableCell>
 								<TableCell align="left">{row.date}</TableCell>
-								<TableCell align="left">{row.status}</TableCell>
+								<TableCell align="left">
+									<span
+										className="status"
+										style={makeStyle(row.status)}
+									>
+										{row.status}
+									</span>
+								</TableCell>
 								<TableCell align="left">Details</TableCell>
 							</TableRow>
 						))}
